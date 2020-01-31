8b1b66e6821f98afe458bf7b6247f7626b82a89a
Justin Chang <justin1992611@gmail.com>
2020-01-31 10:53:32 -0800

    update

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,22 @@
 import React from "react";
+import styled from "styled-components";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  const Card = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: lightcyan;
+    margin: 7% 20%
+    border: solid 1px black;`;
+
+  return (
+    <Card>
+      <h2>{props.name}</h2>
+      <p>{props.species}</p>
+      <p>{props.status}</p>
+    </Card>
+  );
 }
